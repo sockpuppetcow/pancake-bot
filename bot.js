1c3b6295bd938ec3ddfd31e24a171be15337f7a6
Andrew Thompson <sockpuppetcow@gmail.com>
2019-10-10 14:11:58 -0500

    Start on the bot core, module loading should work

--- bot.js
+++ bot.js
@@ -1,13 +1,55 @@
 var fs = require('fs');
+var path = require("path");
+const Discord = require("discord.js");
+
+function loadBotModules() {
+	console.log("Loading modules from directory: " + bot.config.modulesPath);
+	fs.readdir(bot.config.modulesPath, function(err, files) {
+		if (err) {
+			console.log("Error while loading modules: " + err);
+			return;
+		}
+
+		files.forEach(function(f, index) {
+			var modPath = path.join(bot.config.modulesPath, f);
+			var modStat = fs.stat(modPath, function(err, stats) {
+				if (stats.isFile()) {
+					if (path.extname(modPath) === ".js") {
+						var temp = require(modPath);
+						bot.modules[temp.config.name] = temp;
+					}
+				}
+
+				//TODO: Register commands
+				
+				for (key in temp.events) {
+					bot.client.on(key, temp.events[key]);
+				}
+
+				bot.modules[temp.config.name].init(bot);
+			});
+		});
+	});
+}
+
+var bot = {
+	config: {
+		modulesPath: './modules'
+	},
+	client: new Discord.Client(),
+	modules: {},
+	commands: {}
+};
+
+
+
+
+//Discord stuff
+
 var token = fs.readFileSync('token.txt', 'utf8');
 token = token.replace(/\r|\n/g, "")
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on("ready", function() {
+bot.client.on("ready", function() {
 	console.log("Hi, I'm Pancake!");
 });
 
-
-
-client.login(token);
+bot.client.login(token);
