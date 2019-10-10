aeec2afd6a765a76b0fa032dd24c3b309b9ebba1
Andrew Thompson <sockpuppetcow@gmail.com>
2019-10-10 12:46:14 -0500

    Bring back index.js except now it's a launch wrapper which will auto-restart the bot should it crash.

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Pancake Bot launch wrapper started.");
+
+var fork = require("child_process").fork;
+
+var child = fork("./bot.js");
+
+function startChild() {
+	child.on("exit", function(status, signal) {
+		console.log("Bot stopped with status code " + status);
+		if (status) {
+			startChild();
+		}
+	});
+}
+
+startChild();
