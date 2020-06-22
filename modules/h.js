0ed4ff3ad767c018b8d345cc7ab4f72bd17d2d2d
Andrew Thompson <sockpuppetcow@gmail.com>
2020-06-22 14:05:48 -0500

    h

---
+++ modules/h.js
@@ -0,0 +1,32 @@
+//Pancake Bot Database Test Module
+var bot;
+
+module.exports = {
+	config: {
+		name: "Database Test"
+	},
+	commands: {
+		h: function(msg, args) {
+			//msg = Full msg object, as from the message event.
+			//args = Arguments, String[]. args[0] is the first argument after the command itself
+			var h = Math.floor(Math.random() * 2000) + 1;
+			var text = "";
+			var i;
+			for (i = 0; i < h; i++) {
+				var upper = Math.round(Math.random());
+				if (upper) {
+					text += "H";
+				} else {
+					text += "h";
+				}
+			}
+			msg.channel.send(text);
+		}
+	},
+	events: {},
+	init: function(m_bot) {
+		bot = m_bot;
+		console.log("[H] Initializing");
+	}
+};
+
