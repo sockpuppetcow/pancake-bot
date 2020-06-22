8f3962d8a8e17a1c95827c74a5a06cff38a0d8b6
Andrew Thompson <sockpuppetcow@gmail.com>
2020-06-22 14:06:10 -0500

    Add rainbow.js command module because gay

---
+++ modules/rainbow.js
@@ -0,0 +1,22 @@
+//Pancake Bot Database Test Module
+var bot;
+
+module.exports = {
+	config: {
+		name: "Rainbow"
+	},
+	commands: {
+		rainbow: function(msg, args) {
+			//msg = Full msg object, as from the message event.
+			//args = Arguments, String[]. args[0] is the first argument after the command itself
+			var text = ":heart::orange_heart::yellow_heart::green_heart::blue_heart::purple_heart:";
+			msg.channel.send(text);
+		}
+	},
+	events: {},
+	init: function(m_bot) {
+		bot = m_bot;
+		console.log("[Rainbow] Initializing");
+	}
+};
+
