c69363629972ffc14575e0f3daa95c001d5baaf8
sockpuppetcow <sockpuppetcow@gmail.com>
2020-06-24 01:12:12 -0500

    remove test commands from h module

--- modules/h.js
+++ modules/h.js
@@ -24,43 +24,6 @@ module.exports = {
 				msg.channel.send(text);
 			},
 			perm: "SEND_MESSAGES"
-		},
-		h2: {
-			exec: function(msg, args) {
-				//msg = Full msg object, as from the message event.
-				//args = Arguments, String[]. args[0] is the first argument after the command itself
-				var h = Math.floor(Math.random() * 2000) + 1;
-				var text = "";
-				var i;
-				for (i = 0; i < h; i++) {
-					var upper = Math.round(Math.random());
-					if (upper) {
-						text += "H";
-					} else {
-						text += "h";
-					}
-				}
-				msg.channel.send(text);
-			},
-			perm: "ADMINISTRATOR"
-		},
-		h3: {
-			exec: function(msg, args) {
-				//msg = Full msg object, as from the message event.
-				//args = Arguments, String[]. args[0] is the first argument after the command itself
-				var h = Math.floor(Math.random() * 2000) + 1;
-				var text = "";
-				var i;
-				for (i = 0; i < h; i++) {
-					var upper = Math.round(Math.random());
-					if (upper) {
-						text += "H";
-					} else {
-						text += "h";
-					}
-				}
-				msg.channel.send(text);
-			}
 		}
 	},
 	events: {},
