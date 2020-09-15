9460e5f567ba2c9dd4676b27d404065b74c128a6
Andrew Thompson <sockpuppetcow@gmail.com>
2020-09-15 10:04:26 -0500

    Add early fight module

---
+++ modules/fightclub.js
@@ -0,0 +1,46 @@
+//Pancake Bot Fight Club
+var bot;
+
+function generateLine(user, target, damage) {
+	let lines = [
+		user + " slaps " + target + " around a bit with a large trout, dealing " + damage + " damage.",
+		user + " casts magic missile, and hits " + target + " with " + damage + " damage.",
+		user + " punts " + target + " to the moon.",
+		user + " strangles " + target + " dealing " + damage + " damage.",
+		user + " hits " + target + "'s ankles with a wrench, dealing " + damage + " damage.",
+		user + " kicks " + target + " dealing " + damage + " damage.",
+		user + " roundhouse kicks " + target + " dealing " + damage + " damage.",
+		user + " casts fireball at " + target + " and deals " + damage + " damage.",
+		user + " snaps " + target + " out of existence.",
+		user + " tases " + target + " dealing " + damage + " damage.",
+		user + " smashes a TV over " + target + "'s head, dealing " + damage + " damage."
+	];
+
+	return lines[Math.floor(Math.random()*lines.length)];
+}
+
+module.exports = {
+	config: {
+		name: "Fight Club"
+	},
+	commands: {
+		fight: {
+			exec: function(msg, args) {
+				let u = msg.member.displayName;
+				let t = args.join(" ");
+				let d = Math.floor(Math.random()*100)+1;
+				
+				let line = generateLine(u,t,d);
+
+				msg.channel.send(line);
+			},
+			perm: "SEND_MESSAGES"
+		}
+	},
+	events: {},
+	init: function(m_bot) {
+		bot = m_bot;
+		console.log("[Fight Club] Initializing");
+	}
+};
+
